e1885688afcdf643f9a4a096a4ab9ef4a10ba7ed
mspara26 <161856705+mspara26@users.noreply.github.com>
2024-08-22 11:59:00 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,5 +1,5 @@
 // put your AEM publish address here
-const AEM_HOST = 'https://publish-p99452-e916802.adobeaemcloud.com';
+const AEM_HOST = 'https://publish-p121050-e1183639.adobeaemcloud.com';
 
 export default function decorate(block) {
   const slugDiv = block.querySelector('div:nth-child(1)');
